a62e9e52684fbcdeb88082ec2a3e4f012f32fcda
mmmim24 <mustaq24@student.sust.edu>
2022-09-11 22:26:09 +0600

    jsondata

--- src/App.js
+++ src/App.js
@@ -1,32 +1,13 @@
+import React from "react";
 import "./App.sass";
-import { Layout } from "antd";
-import { useEffect, useState } from "react";
-import Spinner from "./utils/Spinner";
-import ProtectedRoutes from "./protected.routes";
-import PublicRoutes from "./public.routes";
-import { useStateValue } from "./state/StateProvider";
+
 
 function App() {
-	const [loading, setLoading] = useState(true);
-	const [{ user }, action] = useStateValue();
 
-	useEffect(() => {
-		//check if user exists
-		setLoading(false);
-	}, []);
 
 	return (
-		<Layout>
-			{loading ? (
-				<Spinner />
-			) : (
-				<>
-					{user && <ProtectedRoutes />}
-					<PublicRoutes />
-				</>
-			)}
-		</Layout>
-	);
-}
+		<div>gg</div>
+		);
+	}
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,24 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.sass";
+// import "./index.sass";
 import App from "./App";
-import { BrowserRouter as Router } from "react-router-dom";
-import reportWebVitals from "./reportWebVitals";
-import { StateProvider } from "./state/StateProvider";
-import reducer, { initialState } from "./state/Reducer";
+// import { BrowserRouter as Router } from "react-router-dom";
+// import reportWebVitals from "./reportWebVitals";
+// import { StateProvider } from "./state/StateProvider";
+// import reducer, { initialState } from "./state/Reducer";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
 	<React.StrictMode>
-		<StateProvider initialState={initialState} reducer={reducer}>
-			<Router>
+		{/* <StateProvider initialState={initialState} reducer={reducer}>
+			<Router> */}
 				<App />
-			</Router>
-		</StateProvider>
+			{/* </Router>
+		</StateProvider> */}
 	</React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
