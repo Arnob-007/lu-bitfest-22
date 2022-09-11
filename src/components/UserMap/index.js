a18474fa1eeab7ebf6482741f9cc1555ecca22ae
Arafat Arnob <arnob001.asa@gmail.com>
2022-09-11 23:53:56 +0600

    Fixed user map

--- src/components/UserMap/index.js
+++ src/components/UserMap/index.js
@@ -1,15 +1,21 @@
 import React from "react";
-import { TileLayer } from "react-leaflet";
+import { MapContainer, TileLayer, useMap } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 
 const UserMap = () => {
+	const position = [24.8949, 91.8687];
 	return (
-		<>
+		<MapContainer
+			className='w-full h-full'
+			center={position}
+			zoom={13}
+			scrollWheelZoom={false}
+		>
 			<TileLayer
 				url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
 				attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
 			/>
-		</>
+		</MapContainer>
 	);
 };
 
