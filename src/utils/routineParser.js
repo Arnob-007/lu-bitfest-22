f4ec370598299b90408654eee472c12ff98f3bc9
nabil <almubinnabil@gmail.com>
2022-09-12 09:59:21 +0600

    routine parser draft

---
+++ src/utils/routineParser.js
@@ -0,0 +1,11 @@
+const csv = require('csv-parser')
+const fs = require('fs')
+const results = [];
+
+
+fs.createReadStream('class_data.csv')
+  .pipe(csv())
+  .on('data', (data) => results.push(data))
+  .on('end', () => {
+    console.log(results[1]);
+  });
